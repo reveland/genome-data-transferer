26f32ca61ca77b91adc594d139b69cf36d8e52d3
reveland <reveland92@gmail.com>
2017-12-28 09:53:55 +0100

    implement dependency_tree endpoint

--- app.py
+++ app.py
@@ -9,70 +9,90 @@ import fitter
 import to_data
 import newick_parser
 
-LOG_FORMAT = '%(levelname)s %(asctime)s - %(message)s'
-logging.basicConfig(filename='./logs/main.log',
-                   level=logging.INFO,
-                   format=LOG_FORMAT)
-console = logging.StreamHandler()
-console.setLevel(logging.DEBUG)
-formatter = logging.Formatter(LOG_FORMAT)
-console.setFormatter(formatter)
-logging.getLogger('').addHandler(console)
 
-logger = logging.getLogger(__name__)
+# LOG_FORMAT = '%(levelname)s %(asctime)s - %(message)s'
+# logging.basicConfig(filename='./logs/main.log',
+#                    level=logging.INFO,
+#                    format=LOG_FORMAT)
+# console = logging.StreamHandler()
+# # console.setLevel(logging.DEBUG)
+# formatter = logging.Formatter(LOG_FORMAT)
+# console.setFormatter(formatter)
+# logging.getLogger('').addHandler(console)
+
+# logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
 baseUrl = 'http://localhost:8080/'
 
-@app.route('/', methods=['GET'])
-def verify():
-    return "Hello world", 200
+# @app.route('/', methods=['GET'])
+# def verify():
+#     return "Hello world", 200
 
-@app.route('/compare', methods=['GET'])
-def compare():
-    headers = {
-        "Content-Type": "application/json"
-    }
-    url = baseUrl + 'getSquares'
-    ref_genome_name = request.args.get('refGenomeName')
-    g1_genome_name = request.args.get('genomeName1')
-    g2_genome_name = request.args.get('genomeName2')
-    logger.info('get genome: %s' % ref_genome_name)
-    ref = json_normalize(json.loads(requests.get(url, params={"genomeName": ref_genome_name}, headers=headers).text))
-    logger.info('received genome: %s with length %d' % (ref_genome_name, len(ref)))
-    logger.info('get genome: %s' % g1_genome_name)
-    g1 = json_normalize(json.loads(requests.get(url, params={"genomeName": g1_genome_name}, headers=headers).text))
-    logger.info('received genome: %s with length %d' % (g1_genome_name, len(g1)))
-    logger.info('get genome: %s' % g2_genome_name)
-    g2 = json_normalize(json.loads(requests.get(url, params={"genomeName": g2_genome_name}, headers=headers).text))
-    logger.info('received genome: %s with length %d' % (g2_genome_name, len(g2)))
-    logger.info('merge genomes')
-    ref_merged, g1_merged, g2_merged = merger.merge_genomes(ref, g1, g2)
-    logger.info('fit genomes')
-    ref_fitter, g1_fitter, g2_fitter = fitter.fit_genomes(ref_merged, g1_merged, g2_merged)
-    logger.info('combinate them')
-    data = to_data.to_data(ref_fitter, g1_fitter, g2_fitter)
-    logger.info('response with data')
-    resp = Response(json.dumps(data))
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
+# @app.route('/compare', methods=['GET'])
+# def compare():
+#     headers = {
+#         "Content-Type": "application/json"
+#     }
+#     url = baseUrl + 'getSquares'
+#     ref_genome_name = request.args.get('refGenomeName')
+#     g1_genome_name = request.args.get('genomeName1')
+#     g2_genome_name = request.args.get('genomeName2')
+#     logger.info('get genome: %s' % ref_genome_name)
+#     ref = json_normalize(json.loads(requests.get(url, params={"genomeName": ref_genome_name}, headers=headers).text))
+#     logger.info('received genome: %s with length %d' % (ref_genome_name, len(ref)))
+#     logger.info('get genome: %s' % g1_genome_name)
+#     g1 = json_normalize(json.loads(requests.get(url, params={"genomeName": g1_genome_name}, headers=headers).text))
+#     logger.info('received genome: %s with length %d' % (g1_genome_name, len(g1)))
+#     logger.info('get genome: %s' % g2_genome_name)
+#     g2 = json_normalize(json.loads(requests.get(url, params={"genomeName": g2_genome_name}, headers=headers).text))
+#     logger.info('received genome: %s with length %d' % (g2_genome_name, len(g2)))
+#     logger.info('merge genomes')
+#     ref_merged, g1_merged, g2_merged = merger.merge_genomes(ref, g1, g2)
+#     logger.info('fit genomes')
+#     ref_fitter, g1_fitter, g2_fitter = fitter.fit_genomes(ref_merged, g1_merged, g2_merged)
+#     logger.info('combinate them')
+#     data = to_data.to_data(ref_fitter, g1_fitter, g2_fitter)
+#     logger.info('response with data')
+#     resp = Response(json.dumps(data))
+#     resp.headers['Access-Control-Allow-Origin'] = '*'
+#     return resp
 
-@app.route('/tree', methods=['GET'])
-def tree():
-    headers = {
-        "Content-Type": "application/json"
-    }
-    url = baseUrl + 'tree'
-    logger.info('get newick from')
-    newick = requests.get(url, headers=headers).text
-    logger.info('receive newick')
+# @app.route('/tree', methods=['GET'])
+# def tree():
+#     headers = {
+#         "Content-Type": "application/json"
+#     }
+#     url = baseUrl + 'tree'
+#     logger.info('get newick from')
+#     newick = requests.get(url, headers=headers).text
+#     logger.info('receive newick')
+#     data = newick_parser.parse_newick(newick)
+#     logger.info('parse newick')
+#     resp = Response(json.dumps(data))
+#     logger.info('respond')
+#     resp.headers['Access-Control-Allow-Origin'] = '*'
+#     return resp
+
+@app.route('/dependency_tree', methods=['GET'])
+def dependency_tree():
+    with open('ba_pom.json', 'r') as infile:
+        ba_pom = json.load(infile)
+    newick = json_to_newick(ba_pom)
+
     data = newick_parser.parse_newick(newick)
-    logger.info('parse newick')
+    
     resp = Response(json.dumps(data))
-    logger.info('respond')
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
+
+def json_to_newick(json, key = lambda d: 'id-' + str(d['id']), scion = lambda d: d['dependencies']):
+    """Convert json data to newick tree format"""
+    if len(scion(json)) == 0:
+        return key(json)
+    else:
+        return '%s(%s):10.0' % (key(json), ','.join(map(json_to_newick, scion(json))))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
--- newick_parser.py
+++ newick_parser.py
@@ -2,23 +2,48 @@ from Bio import Phylo
 from io import StringIO
 import json
 
-def parse_newick(newick_str):
-    tree = Phylo.read(StringIO(newick_str), 'newick')
 
-    height = 100
-    width = 100
-    clades = sorted(list(tree.find_clades()), key=lambda c: tree.clade.distance(c), reverse=True)
-    max_x = tree.clade.distance(clades[0])
-    number_of_leafs = int(len(clades) / 2)
+def get_number_of_leafs(c):
+    return 1 if len(c.clades) == 0 else sum(map(get_number_of_leafs, c.clades))
+
+def add_clade(clade, y, neighbors_leafs_left):
+    global nodes
+    
+    nof = get_number_of_leafs(clade)
+    x = neighbors_leafs_left + (nof - 1) / 2
+    x*=5
+
+    i=0
+    name = clade.name
+    while(clade.name in nodes):
+        clade.name = name + ('' if i == 0 else str(i))
+        i+=1
+    nodes[clade.name] = {'text':clade.name, 'x':x, 'y':y }
+
+    leafs_left = neighbors_leafs_left
+    for c in clade.clades:
+        add_clade(c, y + 10, leafs_left)
+        leafs_left += get_number_of_leafs(c)
+
+def parse_newick(newick_str):
+    global nodes
+    
     nodes = {}
+    # newick_str = 'a(b(c,d,e,f,c,t,y,u,s,d),g(h,j))'
+    tree = Phylo.read(StringIO(newick_str), 'newick')
+    add_clade(tree.clade, 0, 0)
+
+    clades = list(tree.find_clades())
     for i, clade in enumerate(clades):
-        if len(clade.clades) == 0:
-            y = width
-            x = height / number_of_leafs * i
-            nodes[clade.name] = {'text':clade.name, 'nodeId':i, 'x':x, 'y':y, 'children': []}
-        else:
-            y = tree.clade.distance(clade) / max_x * width
-            children = list(map(lambda c: nodes[c.name], clade.clades))
-            x = children[0]['x'] + (children[1]['x'] - children[0]['x']) / 2
-            nodes[clade.name] = {'text':clade.name, 'nodeId':i, 'x':x, 'y':y, 'children': children}
-    return nodes['vert0']
+        children = list(map(lambda c: nodes[c.name], clade.clades))
+        nodes[clade.name]['children'] = children
+
+    return nodes[tree.clade.name]
+
+
+# print(parse_newick('a(b(d:10,e),c)'))
+# print(parse_newick('a(b(d:10,e),c)'))
+
+# tree = Phylo.read(StringIO('a(b(d:10,e:10):10,c:10):10'), 'newick')
+# print(get_number_of_leafs(tree.clade))
+
